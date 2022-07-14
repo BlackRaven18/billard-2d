04ebf6134c062a1799ceb6531ee631d22d92b5bd
Arek Wolski <bebe.@gmail.com>
2022-07-14 09:54:06 +0200

    Nowy projekt

---
+++ desktop/src/com/billard/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.billard;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.billard.Main;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Billard-2D");
+		new Lwjgl3Application(new Main(), config);
+	}
+}
