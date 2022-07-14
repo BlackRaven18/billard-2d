84fca21b502d6b2cfc1744bb11562e17b944ebe6
Arkadiusz Wolski <bebe.@gmail.com>
2022-07-14 15:54:00 +0200

    Creating table as tiled map

---
+++ assets/table.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="table" tilewidth="32" tileheight="32" tilecount="1504" columns="47">
+ <image source="table2 png.png" width="1504" height="1024"/>
+</tileset>
